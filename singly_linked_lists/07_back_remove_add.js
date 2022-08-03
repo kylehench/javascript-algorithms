b0250cdd25dd49f64f1ca39cff2c8a05c5822eb1
Kyle Hench <kylehench@gmail.com>
2022-08-02 17:37:23 -0700

    algo

---
+++ singly_linked_lists/07_back_remove_add.js
@@ -0,0 +1,130 @@
+class Node {
+  constructor(data) {
+    this.data = data
+    this.next = null
+  }
+}
+
+class SLL {
+  constructor() {
+    this.head = null
+  }
+  addFront(data) {
+    let newNode = new Node(data)
+
+    // add head node if head is not present
+    if (!this.head) {
+      this.head = newNode
+      return this
+    }
+
+    // insert new node if head node already present
+    newNode.next = this.head
+    this.head = newNode
+    return this
+  }
+  removeFront() {
+    this.head = this.head.next
+    return this
+  }
+  front() {
+    return this.head.data
+  }
+  print() {
+    console.log(JSON.stringify(this))
+    return this
+  }
+
+  contains(value) {
+    let runner = this.head
+    while (runner!==null) {
+      if (runner.data===value) return true
+      runner = runner.next
+    }
+    return false
+  }
+
+  length() {
+    let runner = this.head
+    let count = 0
+    while (runner!==null) {
+      count++
+      runner = runner.next
+    }
+    return count
+  }
+
+  display() {
+    let runner = this.head
+    let content = ''
+    if (runner!==null) {
+      content += runner.data
+      runner = runner.next
+    }
+    while (runner!==null) {
+      content += `, ${runner.data}`
+      runner = runner.next
+    }
+    console.log(content)
+  }
+
+  max() {
+    let runner = this.head
+    let maximum = runner.data
+    while (runner.next) {
+      runner = runner.next
+      if (runner.data>maximum) maximum = runner.data
+    }
+    return maximum
+  }
+
+  min() {
+    let runner = this.head
+    let minimum = runner.data
+    while (runner.next) {
+      runner = runner.next
+      if (runner.data<minimum) minimum = runner.data
+    }
+    return minimum
+  }
+
+  average() {
+    let runner = this.head
+    let average = runner.data
+    let count = 1
+    while (runner.next) {
+      runner = runner.next
+      count++
+      average = (1-1/count)*average + (1/count)*runner.data
+    }
+    return average
+  }
+
+  back() {
+    let runner = this.head
+    while (runner.next) runner = runner.next
+    return runner.data
+  }
+
+  removeBack() {
+    let runner = this.head
+    while (runner.next.next) runner = runner.next
+    runner.next = null
+    return this.head
+  }
+
+  addBack(value) {
+    let runner = this.head
+    while (runner.next) runner = runner.next
+    runner.next = new Node(value)
+    return this
+  }
+}
+
+SLL1 = new SLL()
+SLL1.addFront(18)
+.addFront(5)
+.addFront(73)
+
+console.log(JSON.stringify(SLL1.removeBack(), 0, 4))
+console.log(JSON.stringify(SLL1.addBack(100), 0, 4))
